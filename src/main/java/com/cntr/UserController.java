bc13dcab835427f5eaf244a68c044c0611c06bff
sayalibarge <sayalibarge2@gmail.com>
2021-01-23 00:34:39 +0530

    create addComplaint method

---
+++ src/main/java/com/cntr/UserController.java
@@ -0,0 +1,66 @@
+package com.app.cntr;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.app.dto.ChangePassword;
+import com.app.entity.Complaint;
+import com.app.entity.ContactUs;
+import com.app.entity.User;
+import com.app.exception.ComplaintNotFoundException;
+import com.app.service.AuthService;
+import com.app.service.SendEmailService;
+import com.app.service.UserServiceInmple;
+
+@RestController
+public class UserController {
+	
+	private String mail="";
+	//Model map;
+	
+	@Autowired
+	private UserServiceInmple userServiceInmple;
+	
+	@Autowired
+	private SendEmailService sendEmailService;
+	
+	@Autowired
+	private AuthService authService;
+	
+	
+	@CrossOrigin
+	@PostMapping("/add/{userId}")
+	public Complaint addComplaint(@PathVariable int userId,@RequestBody Complaint complaint) {
+		
+		userServiceInmple.checkComplaint(complaint);
+		
+		Optional<String> com = Optional.ofNullable(complaint.getComplaintDetail());
+		
+		if(!com.isPresent()) {
+			throw new ComplaintNotFoundException("Please enter complaint detail field");
+		}
+		
+		
+		String today = new Date().getDate()+"-"+(new Date().getMonth() +1)+"-"+"2021";
+		 
+		complaint.setUserId(userId);
+		complaint.setEmployeeId(0);
+		complaint.setComplaintDate(today);
+		complaint.setUserComplaintStatus("Pending");
+		complaint.setComplaintStatus("In Active");
+
+		
+		return userServiceInmple.addComplaint(complaint);
+	}
+
+	
+	
+}
